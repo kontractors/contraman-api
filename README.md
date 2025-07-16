f65e1e438f719b0ddd8cda677df85c4c30a14cd7
416rehman <416rehman@ahmadz.ai>
2025-07-15 22:22:16 -0400

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# ContraMan
+
+## Setup
+```bash
+#1. Clone the repository:
+git clone https://github.com/kontractors/contraman-api.git
+  
+#2. Change into the project directory:
+cd contraman-api
+   
+#3. Install the dependencies:
+npm install
+   
+#4. Start the application:
+npm run dev:hot 
+```
+
+## Dependencies
+
+- express: The main web framework for building the application.
+- pino-http: A logger for HTTP requests, providing structured logging.
---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        ignores: [
+            'dist',
+            'node_modules',
+            'coverage',
+            'build',
+            'public',
+            'out',
+            'lib',
+            'temp',
+            'tmp',
+            '**/*.d.ts',
+        ],
+    }
+);
